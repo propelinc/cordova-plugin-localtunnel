7773b2591de4a4f2670b8df2358e47c410b6175c
Ram Mehta <ram.mehta@gmail.com>
2019-08-07 15:31:37 -0400

    Remove code that sets useragent to IE for PR portal.

--- src/android/LocalTunnel.java
+++ src/android/LocalTunnel.java
@@ -947,11 +947,6 @@ public class LocalTunnel extends CordovaPlugin {
                     settings.setMediaPlaybackRequiresUserGesture(mediaPlaybackRequiresUserGesture);
                 }
 
-                if (url.contains("ebtpr.com")) {
-                    //Force set User Agent to look like MSIE
-                    settings.setUserAgentString("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)");
-                }
-
                 String overrideUserAgent = preferences.getString("OverrideUserAgent", null);
                 String appendUserAgent = preferences.getString("AppendUserAgent", null);
 
